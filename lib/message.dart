82e418df25672739ec625fd1db3bb1f7c3c806fb
R.Noto <rikitonoto@gmail.com>
2023-06-19 21:59:18 +0900

    write the comment and check in message

--- lib/message.dart
+++ lib/message.dart
@@ -5,8 +5,10 @@ import 'dart:typed_data';
 ///
 /// these sections represent meanings each of data in header.
 enum Sections {
-  command,  /// represent the command of the message.
-  bodySize, /// represent the body size of the message.
+  /// represent the command of the message.
+  command,
+  /// represent the body size of the message.
+  bodySize,
 }
 
 /// section strings in a message.
@@ -24,11 +26,13 @@ extension SectionsChars on Sections {
 /// a header class in message
 ///
 /// ## format
-/// **<headerSize>\n[<sectionName>=<sectionData>\n...]**
+/// **\<headerSize\>\n\[\<sectionName\>=\<sectionData\>\n...\]**
 ///
-/// headerSize: the size exclude the header size string itself and a char of LF after that strings of the header.
-/// sectionName: a name that describes represent the meaning of data written after the section name itself.
-/// sectionData: data of a section.
+/// **headerSize**: the size exclude the header size string itself and a char of LF after that strings of the header.
+///
+/// **sectionName**: a name that describes represent the meaning of data written after the section name itself.
+///
+/// **sectionData**: data of a section.
 ///
 /// ### example
 /// this message header has a command section and a body size section.
@@ -39,17 +43,26 @@ extension SectionsChars on Sections {
 /// ```
 ///
 class MessageHeader {
-  static const String delimiter = "\n"; /// a delimiter string in a header.
+  /// a delimiter string in a header.
+  static const String delimiter = "\n";
 
   late final String command;
   late final int bodySize;
-  late final int headerSize;  /// a header size exclude the header size section.
-  late final String rawData;  /// raw data
 
+  /// a header size exclude the header size section.
+  late final int headerSize;
+
+  /// raw data of the header exclude the header size section.
+  late final String rawData;
+
+  /// a content of the header exclude the header size section.
   String get headContent =>
       "${Sections.command.name}=$command\n${Sections.bodySize.name}=$bodySize\n";
+
+  /// a content of the header all.
   String get message => "$headerSize\n$headContent";
 
+  /// constructor from Uint8List(receive data).
   MessageHeader(Uint8List data) {
     String? header = parseHeaderSize(utf8.decode(data));
 
@@ -65,6 +78,7 @@ class MessageHeader {
         int.parse(parseHeaderSection(header, Sections.bodySize.name) ?? "0");
   }
 
+  /// constructor from params(send data).
   MessageHeader.fromParam({
     required this.command,
     required this.bodySize,
@@ -74,9 +88,12 @@ class MessageHeader {
             .length;
   }
 
-  /// parse a section in header from all of header.
-  /// section's format is <Section name>=<content>.
+  /// parse a section from all of header.
   ///
+  /// parse and get section data in [header].
+  /// [header] is all of the header string.
+  /// this function searches a section of [section] in [header], then parses and returns the found data.
+  /// section's format is <Section name>=<content>.
   String? parseHeaderSection(String header, String section) {
     RegExpMatch? valueMatch =
         RegExp('^$section=(.*?)\$', dotAll: true, multiLine: true)
@@ -85,10 +102,12 @@ class MessageHeader {
   }
 
   /// parse header size of the received message.
-  /// return: header content exclude the header size.
-  String? parseHeaderSize(String src) {
+  ///
+  /// parse and set a header size from received message string [message].
+  /// after that return a header string exclude the header size.
+  String? parseHeaderSize(String message) {
     RegExpMatch? headerSizeMatch =
-        RegExp('^([0-9]+)(?:$delimiter(.*))?', dotAll: true).firstMatch(src);
+        RegExp('^([0-9]+)(?:$delimiter(.*))?', dotAll: true).firstMatch(message);
     final String? headerSizeStr = headerSizeMatch?.group(1);
 
     // check it was not match.
@@ -107,11 +126,31 @@ class MessageHeader {
   }
 }
 
+/// a message class
+///
+/// ## format
+/// **\<header\>\[\<body\>\]**
+///
+/// **header**: the header include info of size etc in message.(reference [MessageHeader])
+/// **body**: a body data of message.
+///       if the char count is more than [MessageHeader.bodySize], doesn't recognize more strings.
+///
+/// ### example
+/// this message has a body size section.
+/// it define that a body size is 10byte, but body data has 27byte data.
+/// then this class recognize only 10byte data.
+/// ```dart
+/// const data = "15\nBODY_SIZE=10\n hello, i have 27byte data.";
+/// Message message = Message(Uint8List.fromList(utf8.encode(data)));
+/// print(message.body); // " hello, i "
+/// ```
+///
 class Message {
   late final MessageHeader header;
   late final String body;
   String get message => "${header.message}$body";
 
+  /// constructor from Uint8List(receive data).
   Message(Uint8List data) {
     header = MessageHeader(data);
     // body start position is the header size + the header size char count + LF
@@ -124,6 +163,7 @@ class Message {
     body = utf8.decode(data.sublist(headerEndPosition, bodyEndPosition));
   }
 
+  /// constructor from params(send data).
   Message.fromBody({
     required String command,
     required this.body,
@@ -132,6 +172,7 @@ class Message {
   }
 }
 
+/// exception of format error.
 class MessageFormatException implements Exception {
   const MessageFormatException(this.message);
   final String message;
